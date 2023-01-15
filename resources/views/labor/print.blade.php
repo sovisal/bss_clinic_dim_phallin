86d45ed24d13caa6a5584780dd634cdf6ccf9136
SokunSovisal <sovisalsokun@gmail.com>
2023-01-15 22:05:32 +0700

    fix print error in labor xray and ecg

--- resources/views/labor/print.blade.php
+++ resources/views/labor/print.blade.php
@@ -61,7 +61,7 @@
 			</section>
 		</section>
 		<div class="signature">
-            <div class="text-center">វេជ្ជបណ្ឌិត៖ <span class="KHMOULLIGHT">{{ d_obj($echography, 'doctor', ['name_kh']) }}</span></div>
+            <div class="text-center">វេជ្ជបណ្ឌិត៖ <span class="KHMOULLIGHT">{{ d_obj($labor, 'doctor', ['name_kh']) }}</span></div>
 			<img src="{{ asset('images/site/signature.png') }}" alt="">
 		</div>
 
